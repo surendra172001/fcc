3a0ca452bc7eaadf565119e520f7496eb5349720
Glitch (axiomatic-veiled-catfish) <none>
2020-10-13 08:01:57 +0000

    ❄️🚞 Checkpoint ./server.js:21097030/370 ./package.json:21097030/6078

--- server.js
+++ server.js
@@ -9,10 +9,12 @@ fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'pug');
 
 app.route("/").get((req, res) => {
   //Change the response to render the Pug template
-  res.send(`Pug template is not defined.`);
+  res.render('pug/index');
+  res.render(process.cwd() + "/views/pug/index");
 });
 
 app.listen(process.env.PORT || 3000, () => {
