1afa60c23433c93989a19a041a8b7126767ba8d1
Glitch (axiomatic-veiled-catfish) <none>
2020-10-15 16:28:25 +0000

    📙📒 Authentication strategies

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 "use strict";
+
+// Authentication Strategies
+
 require("dotenv").config();
 const express = require("express");
 const myDB = require("./connection");
@@ -6,6 +9,7 @@ const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const session = require("express-session");
 const passport = require("passport");
 const ObjectID = require("mongodb").ObjectID;
+const LocalStrategy = require("passport-local");
 
 const app = express();
 app.set("view engine", "pug");
@@ -29,14 +33,20 @@ app.use(passport.session());
 
 myDB(async client => {
   const myDataBase = await client.db("database").collection("users");
+  
 
   // Be sure to change the title
   app.route("/").get((req, res) => {
     // Change the response to render the Pug template
     res.render("pug", {
       title: "Connected to Database",
-      message: "Please login"
+      message: "Please login",
+      showLogin: true
     });
+  });
+  
+  app.route("/login").post(passport.authenticate('local', { failureRedirect: '/'}), (req, res) => {
+    res.redirect('/profile');
   });
 
   // Serialization and deserialization here...
@@ -48,7 +58,30 @@ myDB(async client => {
       done(null, doc);
     });
   });
-  // Be sure to add this...
+  
+    passport.use(
+    new LocalStrategy((username, password, done) => {
+      myDataBase.findOne({ username: username }, function(err, user) {
+        console.log("User " + username + " attempted to log in.");
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false);
+        }
+        if (password !== user.password) {
+          return done(null, false);
+        }
+        return done(null, user);
+      });
+    })
+  );
+  
+  app.route("/profile").get((req, res) => {
+    res.render('profile', req.user);
+  });
+
+
 }).catch(e => {
   app.route("/").get((req, res) => {
     res.render("pug", { title: e, message: "Unable to login" });
